05d337e40f31ec1ee9f23ea48070e80df4c281fd
Adm <khost@bk.ru>
2023-05-10 02:39:48 +0300

    Создание и изучение телеграм бота на основе статьи (1 из 5 уроков закончен).

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
 import config
+import logging
 
+# log
+#logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -12,7 +15,14 @@ dp = Dispatcher(bot)
 async def process_start_command(message: types.Message): # аннотация явно указывает тип параметра message
     await message.reply("Привет!\nНапиши мне что-нибудь!")
 
-
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, #skip_updates=False
+)
--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-BOT_TOKEN = "6179443281:AAHOdd9h8KpVLesyFt5TpKo93h0FimFg6-o"
+# https://surik00.gitbooks.io/aiogram-lessons/content/chapter1.html
+# Хорошая статья "Telegram бот на Python 3 и aiogram"
+
+BOT_TOKEN = "6005721035:AAEWkUuCIB14_Ybr8dmuTtQPT_bU-1D-Iro"
 SBER_TEST_TOKEN = "401643678:TEST:66afefaa-2d38-4ac4-a784-b754e2807379"
