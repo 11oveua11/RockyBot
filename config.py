db8a88a6f4e109f76b092d70ec0978b8d673f008
11oveua11 <89461521+11oveua11@users.noreply.github.com>
2023-05-10 10:47:33 +0300

    Update config.py
    Temporary false token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # https://surik00.gitbooks.io/aiogram-lessons/content/chapter1.html
 # Хорошая статья "Telegram бот на Python 3 и aiogram"
 
-BOT_TOKEN = "6005721035:AAEWkUuCIB14_Ybr8dmuTtQPT_bU-1D-Iro"
+BOT_TOKEN = "401643678:TEST:66afefaa-2d38-4ac4-a784-b754e2807379" # ложный токен, для защиты
 SBER_TEST_TOKEN = "401643678:TEST:66afefaa-2d38-4ac4-a784-b754e2807379"
