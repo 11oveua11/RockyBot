b915f2ff361d5a51aad440cbb4298cb6c3d15913
11oveua11 <89461521+11oveua11@users.noreply.github.com>
2023-05-10 10:38:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # https://surik00.gitbooks.io/aiogram-lessons/content/chapter1.html
 # Хорошая статья "Telegram бот на Python 3 и aiogram"
 
-BOT_TOKEN = ""
+BOT_TOKEN = "6005721035:AAEWkUuCIB14_Ybr8dmuTtQPT_bU-1D-Iro"
 SBER_TEST_TOKEN = "401643678:TEST:66afefaa-2d38-4ac4-a784-b754e2807379"
